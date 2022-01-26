37e3157c607797999ef07f958d7e3d83091af297
Andrea-gt <a.ximeram@gmail.com>
2022-01-26 13:49:31 -0600

    Test

---
+++ Radio.java
@@ -0,0 +1,11 @@
+
+public interface Radio {
+boolean encenderApagar();
+String guardarEmisoraActual(int numBoton);
+String seleccionarEmisoraGuardada(int numBoton);
+String cambiarSenal(int opcion);
+void subirEmisora();
+void bajarEmisora();
+float getEmisoraActual();
+boolean comprobarEncendida();
+}
